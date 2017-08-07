5d59b5a80cdd774cb68d244cf0a70f89e9f46739
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-08-07 20:24:00 +0200

    Add pokemon basic block

---
+++ src/Plugin/PokemonBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\hello_world\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "pokemon_block",
+ *   admin_label = @Translation("Pokemon block"),
+ *   category = @Translation("Pokemon"),
+ * )
+ */
+class PokemonBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+}
