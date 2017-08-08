f4ade1cdedf38dd493524e6e1085c79f468da4e3
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-08-08 21:04:33 +0200

    Modify config form to let user choose which resource to show

--- src/Form/PokemonConfigForm.php
+++ src/Form/PokemonConfigForm.php
@@ -9,19 +9,22 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Defines a form that configures forms module settings.
  */
-class PokemonConfigForm extends ConfigFormBase {
+class PokemonConfigForm extends ConfigFormBase 
+{
 
   /**
    * {@inheritdoc}
    */
-  public function getFormId() {
+  public function getFormId() 
+  {
     return 'pokemon_block_admin_settings';
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
+  protected function getEditableConfigNames() 
+  {
     return [
       'pokemon_block.settings',
     ];
@@ -30,20 +33,29 @@ class PokemonConfigForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
+  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) 
+  {
     $config = $this->config('pokemon_block.settings');
-    $form['amount'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Amount of pokemons to display'),
-      '#default_value' => $config->get('amount'),
-    );
+
+    $form['settings']['active'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('What to show?'),
+      '#default_value' => $config->get('resource'),
+      '#options' => [
+        0 => $this->t('Berries'), 
+        1 => $this->t('Items'),
+        2 => $this->t('Pokemon'),
+      ]
+    ];
+
     return parent::buildForm($form, $form_state);
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state) 
+  {
     $values = $form_state->getValues();
     $this->config('pokemon_block.settings')
       ->set('your_message', $values['your_message'])
