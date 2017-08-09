a55b220745c82405c9ef29c539be547b199a71eb
Wout Borghgraef <wout.borghgraef@gmail.com>
2017-08-09 13:44:37 +0200

    Add block config form

--- src/Plugin/Block/PokemonBlock.php
+++ src/Plugin/Block/PokemonBlock.php
@@ -6,8 +6,10 @@ use GuzzleHttp\Client;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Component\Serialization\Json;
+
 
 /**
  * Provides a 'Hello' Block.
@@ -23,7 +25,7 @@ class PokemonBlock extends BlockBase implements ContainerFactoryPluginInterface
 	/**
    	* @var \GuzzleHttp\Client
    	*/
-     private $http_client;
+  private $http_client;
 
   /**
    * @var \Drupal\Core\Config\ConfigFactory
@@ -38,9 +40,52 @@ class PokemonBlock extends BlockBase implements ContainerFactoryPluginInterface
     $this->config_factory = $config_factory;
   }
 
+
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) 
   {
-    return new static($configuration, $plugin_id, $plugin_definition, $container->get('http_client'), $container->get('config.factory'));
+    return new static(
+      $configuration, 
+      $plugin_id, $plugin_definition, 
+      $container->get('http_client'), 
+      $container->get('config.factory'));
+  }
+
+    /**
+   * {@inheritdoc}
+   */
+    public function defaultConfiguration() {
+      return [
+      'count' => 5,
+      ];
+    }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) 
+  {
+    $form['count'] = [
+      '#type' => 'number', 
+      '#title' => $this->t('Amount to display'), 
+      '#default_value' => $this->configuration['count'],
+      ];
+
+      return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) 
+  {
+    if ($form_state->hasAnyErrors()) 
+    {
+      return;
+    }
+    else 
+    {
+      $this->configuration['count'] = $form_state->getValue('count');
+    }
   }
 
   /**
@@ -51,13 +96,13 @@ class PokemonBlock extends BlockBase implements ContainerFactoryPluginInterface
 
     $build = array();
 
-    $resource = $this->config_factory->get('pokemon_block.settings')->get('resource');
+    $resource_name = $this->config_factory->get('pokemon_block.settings')->get('resource');
     
     // When in config we change the resource setting. It doesn't update, only after disabling and enabling the block, or after clearing the caches.
     // But maybe this is normal?
     // Or maybe if we would have had this in the block form it would change for the block immediatly?
     // But often you need general settings NOT in the block form, so I guess then you'd also need to clear the caches. So I guess it's normal?
-    $response = $this->http_client->get("http://pokeapi.co/api/v2/{$resource}/", array('headers' => array('Accept' => 'application/json')));
+    $response = $this->http_client->get("http://pokeapi.co/api/v2/{$resource_name}/", array('headers' => array('Accept' => 'application/json')));
     
     $data = Json::decode($response->getBody());
 
@@ -65,7 +110,12 @@ class PokemonBlock extends BlockBase implements ContainerFactoryPluginInterface
 
     foreach($data['results'] as $resource)
     {
-      // Also depending on resource return different template?
+      $response2 = $this->http_client->get($resource["url"], array('headers' => array('Accept' => 'application/json')));
+
+      $data2 = Json::decode($response2->getBody());
+
+      var_dump($data2);
+
       $build['children'][$i] = ['#theme' => 'pokemon_block_item', '#data' => $resource ];
       $i++;
     }
